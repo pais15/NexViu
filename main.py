180b551d88df4fe1750d21c4607a0ebd6bda5b82
ali <148367372+pais15@users.noreply.github.com>
2025-12-10 20:21:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Const import *
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
-    await message.reply("سلام! حالا دیگه هیچ وقت فلود نمی‌شم 🎉")
+    await message.reply("سلام! چکاری از دستم بر میاد؟ 🎉")
 
 if __name__ == "__main__":
     app.run()
