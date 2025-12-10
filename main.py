ad7f50fb264695c3fd0f391b07b03fb52059cb0f
ali <148367372+pais15@users.noreply.github.com>
2025-12-10 18:35:23 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Client(
     "NexViu",
     api_id=int(os.getenv("API_ID")),
     api_hash=os.getenv("API_HASH"),
-    bot_token=os.getenv("TOKEN"),
+    bot_token=os.getenv("BOT_TOKEN"),
     workdir="/app", 
     in_memory=False
 )
