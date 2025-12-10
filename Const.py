fb2745baa0a7a7f9135b6e7e9a19cef1e6ad937e
ali <148367372+pais15@users.noreply.github.com>
2025-12-10 20:33:31 +0330

    Update Const.py

--- Const.py
+++ Const.py
@@ -2,13 +2,13 @@ from dataManager import *
 
 app = Client(
     "NexViu",
-    bot_token=os.getenv("BOT_TOKEN"),
+    bot_token=os.getenv("TOKEN"),
     api_id=int(os.getenv("API_ID")),
     api_hash=os.getenv("API_HASH"),
-
+    workdir="/app", 
+    in_memory=False
 )
-"""    workdir="/app", 
-    in_memory=False"""
+
 db_connect = {
     "user": os.getenv("USERDB"),
     "password": os.getenv("PDB"),
