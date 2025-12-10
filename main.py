d776c58a3b09752cd954bcc96a3605dd8cd9d285
ali <148367372+pais15@users.noreply.github.com>
2025-12-10 20:05:36 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from imports import *
 
 load_dotenv()
 
+db_connect = {
+    "user": os.getenv("USERDB"),
+    "password": os.getenv("PDB"),
+    "host": os.getenv("HOSTDB"),
+    "port": os.getenv("PORTDB"),
+    "database": os.getenv("NAMEDB"),
+}
+db = Database(**db_connect)
 app = Client(
     "NexViu",
     api_id=int(os.getenv("API_ID")),
@@ -17,5 +25,4 @@ async def start(client, message):
     await message.reply("سلام! حالا دیگه هیچ وقت فلود نمی‌شم 🎉")
 
 if __name__ == "__main__":
-    app.run() 
-
+    app.run()
