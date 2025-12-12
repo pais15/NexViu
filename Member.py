61e2878c8b59d0dbc9e467fd6216feb199925a9f
ali <148367372+pais15@users.noreply.github.com>
2025-12-12 19:11:41 +0330

    add show info

--- Member.py
+++ Member.py
@@ -44,18 +44,71 @@ async def collaborate_with_us(client:Client, m: Message):
     await m.reply(f'''🎉 **سلام! آماده *درآمد میلیونی* هستی؟** 💸
 
 🔥 **فقط ۳ قدم ساده:**
-1️⃣ لینک زیر رو برای *دوستات* بفرست
-2️⃣ اونا *ثبت‌نام* کنن  
+1️⃣ لینک زیر رو برای **دوستات** بفرست
+2️⃣ اونا **ثبت‌نام** کنن  
 3️⃣ **تو + اونا = سود دوطرفه!** 🚀
 
-⚡ **نکته طلایی:** وقتی دوستت *فعال* بشه، *پاداشت* فوری واریز میشه!
+⚡ **نکته طلایی:** وقتی دوستت فعال بشه، پاداشت فوری واریز میشه!
 
-🌟 **لینک *خاص* تو:**
-{url}
+🌟 [**فقط با یک کلیک شروع کن!**]({url})
 
-**🚀 همین حالا شروع کن! 💰**''')
+**🚀 پول در انتظارته! فرصت و از دست نده! 💰**''')
 
 
 @app.on_message(exists_filter & filters.regex(r"^ℹ️ آمار، گزارش و رویدادها$"))
-async def stats_reports_events(client:Client, m: Message):
-    pass
+async def stats_reports_events(client: Client, m: Message):
+    user_info = await db.select('users', ['userID', 'name', 'family', 'card'], {'userID': str(m.chat.id)})
+    userID = user_info[0]['userID']
+
+    default_user = await client.get_users(m.chat.id)
+
+    full_name_db = f"{user_info[0]['name'] or ''} {user_info[0]['family'] or ''}".strip()
+    display_name = full_name_db if full_name_db else default_user.first_name
+    userName = f"[{display_name}](tg://user?id={userID})"
+
+    card = user_info[0]['card'] or 'تعریف نشده'
+
+    wallet = await db.select('wallet', ['coins'], {'userID': userID})
+    coins = wallet[0]['coins']
+
+    posts = await db.select('post', ['postID'], {'userID': userID})
+
+    text = f"""
+**👤 اطلاعات کاربر**
+- نام: {userName}
+- موجودی: **{coins:,} تومان**
+- کارت بانکی: `{card}`
+
+"""
+
+    channels = await db.select('channel', ['title', 'link'], {'userID': userID})
+    if channels:
+        text += "**📢 کانال‌ها و گروه‌های ثبت‌شده:**\n"
+        for ch in channels:
+            title = ch["title"]
+            link = ch["link"]
+            text += f"- [{title}]({link})\n"
+        text += "\n"
+
+    if posts:
+        text += "**📝 پست‌های ارسال‌شده:**\n"
+        post_lines = []
+
+        for post in posts:
+            postID = post['postID']
+            sent_posts = await db.select(
+                'create_post', 
+                ['channelID', 'textID', 'catgID', 'status'], 
+                {'postID': postID}
+            )
+
+            for num, sent in enumerate(sent_posts, start=1):
+                if sent['status'] == 'sent':
+                    channel = await db.select('channel', ['link'], {'channelID': sent['channelID']})
+                    link = channel[0]['link']
+                    post_lines.append(f"- پست **{num}** → [مشاهده در کانال]({link})")
+
+        if post_lines:
+            text += "\n".join(post_lines)
+
+    await m.reply(text, parse_mode="markdown")
